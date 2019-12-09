403bf82b7f6bf3ddee392f32e2af94af6d0e06a6
Matt <mp.westra@student.avans.nl>
2019-12-09 14:08:10 +0100

    Added location permission request

--- app/src/main/java/com/example/vananaarbreda/Activities/MapsActivity.java
+++ app/src/main/java/com/example/vananaarbreda/Activities/MapsActivity.java
@@ -1,9 +1,15 @@
 package com.example.vananaarbreda.Activities;
 
+import androidx.annotation.NonNull;
+import androidx.core.app.ActivityCompat;
+import androidx.core.content.ContextCompat;
 import androidx.fragment.app.FragmentActivity;
 
+import android.Manifest;
+import android.content.pm.PackageManager;
 import android.content.res.Configuration;
 import android.os.Bundle;
+import android.widget.Toast;
 
 import com.example.vananaarbreda.Map.MapHandler;
 import com.example.vananaarbreda.R;
@@ -16,20 +22,30 @@ import com.google.android.gms.maps.SupportMapFragment;
 import com.google.android.gms.maps.UiSettings;
 import com.google.android.gms.maps.model.LatLng;
 
+import java.security.Permissions;
+
 public class MapsActivity extends FragmentActivity implements OnMapReadyCallback {
 
     private GoogleMap mMap;
     private Route route;
-    private static  LatLng BREDA = new LatLng(51.5719149, 4.768323);
+    private static final LatLng BREDA = new LatLng(51.5719149, 4.768323);
+    private static final int PERMISSIONS_REQUEST_ACCESS_FINE_LOCATION = 1;
+    private static boolean mLocationPermissionGranted;
 
     @Override
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_maps);
         // Obtain the SupportMapFragment and get notified when the map is ready to be used.
+        if (ContextCompat.checkSelfPermission(this, Manifest.permission.ACCESS_FINE_LOCATION) != PackageManager.PERMISSION_GRANTED) {
+            ActivityCompat.requestPermissions(this, new String[]{Manifest.permission.ACCESS_FINE_LOCATION}, PERMISSIONS_REQUEST_ACCESS_FINE_LOCATION);
+        }
+
         SupportMapFragment mapFragment = (SupportMapFragment) getSupportFragmentManager()
                 .findFragmentById(R.id.map);
         mapFragment.getMapAsync(this);
+
+        Toast.makeText(this, "Gewiegeird", Toast.LENGTH_LONG).show();
     }
 
 
@@ -59,5 +75,21 @@ public class MapsActivity extends FragmentActivity implements OnMapReadyCallback
         route.addCoordinate(new Coordinate(51.5719149, 4.768323));
         MapHandler.getInstance(mMap).buildWaypoints(route);
         MapHandler.getInstance(mMap).buildRoute(route);
+    }
+
+    @Override
+    public void onRequestPermissionsResult(int requestCode, @NonNull String permissions[], @NonNull int[] grantResults) {
+        mLocationPermissionGranted = false;
+        switch (requestCode) {
+            case PERMISSIONS_REQUEST_ACCESS_FINE_LOCATION: {
+
+                // If request is cancelled, the result arrays are empty.
+                if (grantResults.length > 0 && grantResults[0] == PackageManager.PERMISSION_GRANTED) {
+                    mLocationPermissionGranted = true;
+
+                }
+            }
+        }
+//        updateLocationUI();
     }
 }
