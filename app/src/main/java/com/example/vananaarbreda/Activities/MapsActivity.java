f6cdbc375b7d05819ee4dfe1e24cf0d454083402
Pascal de Bruijn <p.debruijn1@student.avans.nl>
2019-12-09 12:32:53 +0100

    Added standard google map UI settings

--- app/src/main/java/com/example/vananaarbreda/Activities/MapsActivity.java
+++ app/src/main/java/com/example/vananaarbreda/Activities/MapsActivity.java
@@ -10,6 +10,7 @@ import com.google.android.gms.maps.CameraUpdateFactory;
 import com.google.android.gms.maps.GoogleMap;
 import com.google.android.gms.maps.OnMapReadyCallback;
 import com.google.android.gms.maps.SupportMapFragment;
+import com.google.android.gms.maps.UiSettings;
 import com.google.android.gms.maps.model.LatLng;
 import com.google.android.gms.maps.model.MarkerOptions;
 
@@ -46,5 +47,12 @@ public class MapsActivity extends FragmentActivity implements OnMapReadyCallback
         LatLng sydney = new LatLng(-34, 151);
         mMap.addMarker(new MarkerOptions().position(sydney).title("Marker in Sydney"));
         mMap.moveCamera(CameraUpdateFactory.newLatLng(sydney));
+
+        UiSettings settings = googleMap.getUiSettings();
+        settings.setZoomControlsEnabled(true);
+        settings.setTiltGesturesEnabled(true);
+        settings.setRotateGesturesEnabled(true);
+        settings.setMyLocationButtonEnabled(true);
+        settings.setMapToolbarEnabled(true);
     }
 }
