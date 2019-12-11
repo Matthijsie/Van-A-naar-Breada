084a06e7c58a6ee9a000990d3e7ea1738fc41572
Fire <pascaldebruijngsm@gmail.com>
2019-12-11 21:10:05 +0100

    Added Standerd camera zoom

--- app/src/main/java/com/example/vananaarbreda/Activities/MapsActivity.java
+++ app/src/main/java/com/example/vananaarbreda/Activities/MapsActivity.java
@@ -57,5 +57,7 @@ public class MapsActivity extends FragmentActivity implements OnMapReadyCallback
         Route route = new Route();
         route.addCoordinate(new Coordinate(4.768323, 51.5719149));
         MapHandler.getInstance(mMap).buildRoute(route);
+
+        mMap.setMinZoomPreference(16);
     }
 }
