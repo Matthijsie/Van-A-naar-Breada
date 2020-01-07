407ffa646aed466b64b044d8a8ef48eb02597c50
Matt <mp.westra@student.avans.nl>
2020-01-07 14:11:40 +0100

    Changed button for switch

--- app/src/main/java/com/example/vananaarbreda/Activities/HelpActivity.java
+++ app/src/main/java/com/example/vananaarbreda/Activities/HelpActivity.java
@@ -3,30 +3,30 @@ package com.example.vananaarbreda.Activities;
 import androidx.appcompat.app.AppCompatActivity;
 
 import android.os.Bundle;
+import android.util.Log;
+import android.view.View;
+import android.widget.Button;
 import android.widget.CompoundButton;
 import android.widget.Switch;
 
 import com.example.vananaarbreda.R;
 
 public class HelpActivity extends AppCompatActivity {
-
-    private Switch colourBlindSwitch;
+    private static final String TAG = HelpActivity.class.getSimpleName();
+    private Button colourBlindButton;
 
     @Override
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_help);
 
-        colourBlindSwitch = findViewById(R.id.switchColourBlindMode);
-        colourBlindSwitch.setOnCheckedChangeListener(new CompoundButton.OnCheckedChangeListener() {
+        colourBlindButton = findViewById(R.id.buttonColourBLindMode);
+        colourBlindButton.setOnClickListener(new View.OnClickListener() {
             @Override
-            public void onCheckedChanged(CompoundButton buttonView, boolean isChecked) {
-
-                if (isChecked){
-                    getApplication().setTheme(R.style.ColourBlindTheme);
-                }else {
-                    getApplication().setTheme(R.style.AppTheme);
-                }
+            public void onClick(View v) {
+                Log.d(TAG, "colourBlindButton Pressed");
+                getApplication().setTheme(R.style.ColourBlindTheme);
+                recreate();
             }
         });
     }
